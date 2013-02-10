8486890596c0ee145e12b14588cc78d9053e8577
michaellin <michaelv03@gmail.com>
2013-02-09 20:52:55 -0800

    changing player

--- src/player.c
+++ src/player.c
@@ -35,37 +35,35 @@ Coord *placeShip_human(GameState *state, int len) {
     int tyCoor = -1;
     printf("Place a ship of length %d\n", len);
     do {
-        while (!hxCoor = -1) {
-            printf("Gimme the head's X coord (<10) :\n");
-            int hxCoor = atoi(gets(input));
-            if (hxCoor < 0 || hxCoor >= BOARD_WIDTH) {
-                hxCoor = -1;
-            }
-        }
-        while (!hyCoor = -1) {
-            printf("Gimme the head's Y coord (<10) :\n");
-            int hyCoor = atoi(gets(input));
-            if (hyCoor < 0 || hyCoor >= BOARD_HEIGTH) {
-                hyCoor = -1;
-            }
-        }
-        while (!txCoor = -1) {
-            printf("Gimme the tail's X coord (<10) :\n");
-            int txCoor = atoi(gets(input));
-            if (txCoor < 0 || txCoor >= BOARD_WIDTH) {
-                txCoor = -1;
-            }
-        }
-        while (!tyCoor = -1) {
-            printf("Gimme the tail's Y coord (<10) :\n");
-            int tyCoor = atoi(gets(input));
-            if (tyCoor < 0 || tyCoor >= BOARD_HEIGTH) {
-                tyCoor = -1;
-            }
-        }
-    } while (hxCoor != txCoor || hyCoor != tyCoor); //Check if the coords are not diagonal
+        printf("For head of ship\n");
+        Coord head = ask_for_coord();
+        printf("For tail of ship\n");
+        Coord tail = ask_for_coord();
+    } while (head.x != tail.x || head.y != tail.y); //Check if the coords are not diagonal
     Coord *arrOfShips = genShipsArr(hxCoor, hyCoor, txCoor, tyCoor);
 }
+
+Coord* ask_for_coord() {
+    Coord c = NULL;
+    c.x = -1;
+    c.y = -1;
+    while (!c.x = -1) {
+        printf("Gimme the X coord (<10) :\n");
+        int c.x = atoi(gets(input));
+        if (c.x < 0 || c.x >= BOARD_WIDTH) {
+            c.x = -1;
+        }
+    }
+    while (!c.y = -1) {
+        printf("Gimme the Y coord (<10) :\n");
+        int c.y = atoi(gets(input));
+        if (c.y < 0 || c.y >= BOARD_HEIGTH) {
+            c.y = -1;
+        }
+    }
+    return &c;
+}
+    
 
 Coord *attack_human(GameState *state) {
   
